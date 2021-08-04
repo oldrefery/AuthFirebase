b9baee7c86077a7f6be68c82a5080b56e2808461
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-08-04 12:07:12 +0300

    feat: Add firebase initialization and lib

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import firebase from 'firebase';
+
 import Header from './components/Header';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyDW6f7kE7KGQRpkQt3ZjPDe1cgylxReCik',
+  authDomain: 'authentification-7b3b3.firebaseapp.com',
+  projectId: 'authentification-7b3b3',
+  storageBucket: 'authentification-7b3b3.appspot.com',
+  messagingSenderId: '715415067738',
+  appId: '1:715415067738:web:c0628c292a7bad099f8c63',
+};
+
 const App = () => {
+  useEffect(() => {
+    firebase.initializeApp(firebaseConfig);
+  }, []);
   return (
     <SafeAreaView>
       <Header title={'Authorisation'} />
