a7599262b979e6fd81086225250f602ab194d862
oldrefery <35261486+oldrefery@users.noreply.github.com>
2021-08-04 22:39:37 +0300

    style: Change Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Header = ({title}) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Text style={styles.header}>{title}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  header: {alignSelf: 'center', fontSize: 18},
+});
 
 export default Header;
